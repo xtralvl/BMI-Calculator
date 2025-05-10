a34dd7e1799399dfb4ab8aac39d031217b8031fd
Roland Szucs <rolandszucs.private@gmail.com>
2025-05-10 15:49:21 +0200

    hint message colors

--- script.js
+++ script.js
@@ -118,93 +118,100 @@ const calculateBMI = () => {
   if (document.body.classList.contains("male-mode") && (document.getElementById('age').value <= 39)) {
     if (bmi < 18.5) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Underweight).`;
-        hint.innerHTML = `Try adding protein-rich snacks to your day. Light strength training can support healthy weight gain.`;
+        hint.innerHTML = `Try adding protein-rich snacks to your day.<br>Light strength training can support healthy weight gain.`;
       } else if ((bmi > 18.5 ) && (bmi < 24.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Normal weight)`;
-        hint.innerHTML = `Keep up regular activity and balanced meals. Consider adding cardio for heart health.`;
+        hint.innerHTML = `Keep up regular activity and balanced meals.<br>Consider adding cardio for heart health.`;
       } else if ((bmi > 25 ) && (bmi < 29.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Overweight)`;
-        hint.innerHTML = `Swap sugary drinks for water. Try a 20-minute walk after meals.`;
+        hint.innerHTML = `Swap sugary drinks for water.<br>Try a 20-minute walk after meals.`;
       } else if ((bmi > 30 ) && (bmi < 34.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 1))`;
-        hint.innerHTML = `Focus on small daily changes, like cooking at home. Talk to a professional if you feel stuck.`;
+        hint.innerHTML = `Focus on small daily changes, like cooking at home.<br> Talk to a professional if you feel stuck.`;
       } else if ((bmi > 35 ) && (bmi < 39.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 2))`;
-        hint.innerHTML = `Focus on small daily changes, like cooking at home. Talk to a professional if you feel stuck.`;
+        hint.innerHTML = `Focus on small daily changes, like cooking at home.<br>Talk to a professional if you feel stuck.`;
       } else if (bmi >= 40 ) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 3))`;
-        hint.innerHTML = `Focus on small daily changes, like cooking at home. Talk to a professional if you feel stuck.`;
-      }          
-  }
+        hint.innerHTML = `Focus on small daily changes, like cooking at home.<br>Talk to a professional if you feel stuck.`;
+      }
+      
+   }
 
     // Male Age 40+
     if (document.body.classList.contains("male-mode") && (document.getElementById('age').value > 39)) {
         if (bmi < 18.5) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Underweight)`;
-            hint.innerHTML = `Add healthy fats like nuts or olive oil to meals. Monitor bone health with regular checkups.`;
+            hint.innerHTML = `Add healthy fats like nuts or olive oil to meals.<br>Monitor bone health with regular checkups.`;
           } else if ((bmi > 18.5 ) && (bmi < 24.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Normal weight)`;
-            hint.innerHTML = ` Keep moving—daily walks help maintain mobility. Fiber is your friend for digestion.`;
+            hint.innerHTML = ` Keep moving—daily walks help maintain mobility.<br>Fiber is your friend for digestion.`;
           } else if ((bmi > 25 ) && (bmi < 29.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Overweight)`;
-             hint.innerHTML = `Watch portion sizes and reduce processed foods. Try gentle exercise like swimming or yoga.`;
+             hint.innerHTML = `Watch portion sizes and reduce processed foods. <br>Try gentle exercise like swimming or yoga.`;
           } else if ((bmi > 30 ) && (bmi < 34.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 1))`;
-            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health. Limit added sugar and alcohol.`;
+            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health.<br> Limit added sugar and alcohol.`;
           } else if ((bmi > 35 ) && (bmi < 39.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 2))`;
-            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health. Limit added sugar and alcohol.`;
+            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health. <br>Limit added sugar and alcohol.`;
           } else if (bmi >= 40 ) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 3))`;
-            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health. Limit added sugar and alcohol.`;
-          }          
-      }
+            hint.innerHTML = `Aim for gradual progress—5% weight loss can already boost health.<br> Limit added sugar and alcohol.`;
+          }   
+          
+      
+    }
 
         // Female Age 18–39
   if (document.body.classList.contains("female-mode") && (document.getElementById('age').value <= 39)) {
     if (bmi < 18.5) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Underweight)`;
-        hint.innerHTML = `Add calorie-rich but healthy snacks like avocado or nut butter. Make sure you’re getting enough iron.`;
+        hint.innerHTML = `Add calorie-rich but healthy snacks like avocado or nut butter. <br>Make sure you’re getting enough iron.`;
       } else if ((bmi > 18.5 ) && (bmi < 24.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Normal weight)`;
-        hint.innerHTML = `Keep a good sleep schedule and move daily. Stay hydrated—your body needs it.`;
+        hint.innerHTML = `Keep a good sleep schedule and move daily. <br>Stay hydrated—your body needs it.`;
       } else if ((bmi > 25 ) && (bmi < 29.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Overweight)`;
-        hint.innerHTML = `Add veggies to every meal. Dancing or brisk walks can make exercise fun.`;
+        hint.innerHTML = `Add veggies to every meal.<br> Dancing or brisk walks can make exercise fun.`;
       } else if ((bmi > 30 ) && (bmi < 34.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 1))`;
-        hint.innerHTML = `Small habits add up—start with one healthy change. Avoid skipping meals; eat balanced portions.`;
+        hint.innerHTML = `Small habits add up—start with one healthy change. <br>Avoid skipping meals; eat balanced portions.`;
       } else if ((bmi > 35 ) && (bmi < 39.9)) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 2))`;
-        hint.innerHTML = `Small habits add up—start with one healthy change. Avoid skipping meals; eat balanced portions.`;
+        hint.innerHTML = `Small habits add up—start with one healthy change. <br>Avoid skipping meals; eat balanced portions.`;
       } else if (bmi >= 40 ) {
         result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 3))`;
-        hint.innerHTML = `Small habits add up—start with one healthy change. Avoid skipping meals; eat balanced portions.`;
-      }          
+        hint.innerHTML = `Small habits add up—start with one healthy change. <br>Avoid skipping meals; eat balanced portions.`;
+      }
+      
+
   }
 
     // Female Age 40+
     if (document.body.classList.contains("female-mode") && (document.getElementById('age').value > 39)) {
         if (bmi < 18.5) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Underweight)`;
-            hint.innerHTML = `Bone density matters—include calcium-rich foods. Smoothies can help boost intake without big meals.`;
+            hint.innerHTML = `Bone density matters—include calcium-rich foods.<br> Smoothies can help boost intake without big meals.`;
           } else if ((bmi > 18.5 ) && (bmi < 24.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Normal weight)`;
-            hint.innerHTML = `Strength training 2x a week supports muscle and bone. Stay consistent with meals and rest.`;
+            hint.innerHTML = `Strength training 2x a week supports muscle and bone.<br> Stay consistent with meals and rest.`;
           } else if ((bmi > 25 ) && (bmi < 29.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Overweight)`;
-            hint.innerHTML = `Limit refined carbs and sugary snacks. Try cooking more at home.`;
+            hint.innerHTML = `Limit refined carbs and sugary snacks.<br> Try cooking more at home.`;
           } else if ((bmi > 30 ) && (bmi < 34.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 1))`;
-            hint.innerHTML = `Set gentle, realistic goals. Reducing stress can also support weight changes.`;
+            hint.innerHTML = `Set gentle, realistic goals. <br>Reducing stress can also support weight changes.`;
           } else if ((bmi > 35 ) && (bmi < 39.9)) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 2))`;
-            hint.innerHTML = `Set gentle, realistic goals. Reducing stress can also support weight changes.`;
+            hint.innerHTML = `Set gentle, realistic goals.<br> Reducing stress can also support weight changes.`;
           } else if (bmi >= 40 ) {
             result.innerHTML = `Your BMI is ${bmi.toFixed(1)} (Obesity (Class 3))`;
-            hint.innerHTML = `Set gentle, realistic goals. Reducing stress can also support weight changes.`;
-          }          
-          }
+            hint.innerHTML = `Set gentle, realistic goals.<br> Reducing stress can also support weight changes.`;
+          }    
+          
+    
+    }
 
   result.style.fontSize = '1.3rem';
 }
@@ -219,7 +226,7 @@ form.addEventListener('submit', (event) => {
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', () => {
-  document.getElementById('age').value = '';  
+  document.getElementById('age').value = ''; 
   document.getElementById('weight').value = '';  
   document.getElementById('height').value = '';
   document.getElementById("height").disabled = true;
